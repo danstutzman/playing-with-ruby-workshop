60ca3776d06f77a32a5154eed39a3a040fb1ec0c
Daniel Stutzman <dtstutz@gmail.com>
2012-09-09 11:48:30 -0600

    Added 100x120px cropped and scaled photos instead of giving out hot-links.

--- 2_write_to_html.rb
+++ 2_write_to_html.rb
@@ -9,7 +9,7 @@ puts "<body>"
 for cyclist in cyclists
   name = cyclist[:first_name]
 
-  puts "<div style='width:100px;height:100px;float:left'>"
+  puts "<div style='width:110px;height:160px;float:left'>"
   puts "#{name}<br/>"
   puts "</div>"
 end
